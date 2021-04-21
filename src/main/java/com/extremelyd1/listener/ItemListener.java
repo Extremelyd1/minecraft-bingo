da1aa05aa3709bd70650f6d3563233daf3a2eb97
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2021-04-21 17:54:25 +0100

    add milk/lava/water bucket filling, fish bucket filling and mooshroom
    Material collection will be triggered when a player fills their bucket (milk, water, lava), right clicks a fish with a water bucket (fish in a bucket material) and right clicking a mooshroom with a bowl giving stew.

--- src/main/java/com/extremelyd1/listener/ItemListener.java
+++ src/main/java/com/extremelyd1/listener/ItemListener.java
@@ -2,6 +2,7 @@ package com.extremelyd1.listener;
 
 import com.extremelyd1.game.Game;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,8 +10,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.FurnaceExtractEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.*;
 
 public class ItemListener implements Listener {
 
@@ -53,4 +56,55 @@ public class ItemListener implements Listener {
             e.setCancelled(true);
         }
     }
+
+    /**
+     * Caters for collecting milk, water or lava
+     *
+     * @param e - PlayerBucketFillEvent
+     */
+    @EventHandler
+    public void onBucketFill(PlayerBucketFillEvent e) {
+        if (!game.getState().equals(Game.State.IN_GAME)) {
+            e.setCancelled(true);
+            return;
+        }
+
+        game.onMaterialCollected(e.getPlayer(), e.getItemStack().getType());
+    }
+
+    /**
+     * This event caters for the collection of fishes into buckets, and the collection of mushroom stew from mooshrooms
+     *
+     * @param e - PlayerInteractAtEntityEvent
+     */
+    @EventHandler
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) {
+        if (e.isCancelled()) return;
+
+        Map<EntityType, Material> fishes = new HashMap<EntityType, Material>() {{
+           put(EntityType.SALMON, Material.SALMON_BUCKET);
+           put(EntityType.COD, Material.COD_BUCKET);
+           put(EntityType.PUFFERFISH, Material.PUFFERFISH_BUCKET);
+           put(EntityType.TROPICAL_FISH, Material.TROPICAL_FISH_BUCKET);
+        }};
+
+        Map<EntityType, Material> foodHarvesting = new HashMap<EntityType, Material>() {{
+           put(EntityType.MUSHROOM_COW, Material.MUSHROOM_STEW);
+        }};
+
+        if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.WATER_BUCKET ||
+                e.getPlayer().getInventory().getItemInOffHand().getType() == Material.WATER_BUCKET) {
+            Material collectedMaterial = fishes.get(e.getRightClicked().getType());
+            if (collectedMaterial != null) game.onMaterialCollected(e.getPlayer(), collectedMaterial);
+            return;
+        }
+
+        if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.BOWL ||
+                e.getPlayer().getInventory().getItemInOffHand().getType() == Material.BOWL) {
+            Material collectedMaterial = foodHarvesting.get(e.getRightClicked().getType());
+            if (collectedMaterial != null) game.onMaterialCollected(e.getPlayer(), collectedMaterial);
+            return;
+        }
+    }
+
 }
